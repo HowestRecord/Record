3fdd7325fddb4798cc8a9afc1c57d67489690387
KSelsHowest <kris.sels@student.howest.be>
2020-12-19 13:28:46 +0100

    Final

--- record-Dapp/record-contract/contracts/Record.sol
+++ record-Dapp/record-contract/contracts/Record.sol
@@ -1,58 +1,224 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import  '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 
 contract Record is ERC1155  {
- using SafeMath for uint256;
+  enum AssetShareTokenStatus {MINTED, DEVIDED}
 
-  uint256 ASSET_TOKEN_ADD = 1;
-
-  uint256 private _assetTokenIdCount = 0;
-
-  mapping(uint256 => uint256) private _assetTokenIdHolderCount;
-
-  mapping(uint256 => mapping(uint256 => address)) private _assetTokenIdHolder;
-
-  mapping(uint256 => mapping(address => bool)) private  _isAssetTokenIdHolder;
-
-  mapping(string => uint256)  private _mintedAssetTokens;  //Link token ref with assetTokenID
-
-  mapping(string => bool)  private _assetTokenMinted;  
-
-  mapping(uint256 => string)  private _mintedAssetTokensRefs;  //token id ref
-
-  event assetMinted(string indexed assetRef, uint256 indexed assetTokenId);
-
-  constructor(string memory _uri) public ERC1155(_uri) {
-    
+  struct AssetShareToken {
+    uint256 id;
+    address creator;
+    AssetShareTokenStatus status;
+    uint256 shares;
+    string metadataUri;
+    address[] holders;
   }
 
-  function mintAsset(string memory assetRef) public  {
-    require(!assetAllReadyMinted(assetRef), "Asset with with DID is allready minted!");
+  mapping(string => AssetShareToken) public assetShareTokens;
 
-    address assetMinter = msg.sender;
+  string[] public assetRefs;
 
-    uint256 assetTokenIdCount = _assetTokenIdCount;
-    _assetTokenIdCount = _assetTokenIdCount.add(ASSET_TOKEN_ADD);
+  mapping(address => mapping(uint256 => bool)) private _ownedAssetShares;
 
-    _mintedAssetTokens[assetRef] = assetTokenIdCount; //Link asset with tokens
-    _mintedAssetTokensRefs[assetTokenIdCount] = assetRef;
-    _assetTokenMinted[assetRef] = true;
+  mapping(uint256 => string) private _tokenAssetRefs;
 
-    _mint(assetMinter, assetTokenIdCount, 1000, ""); //mint nbrOfAssetTokens for the asset x
+  constructor(string memory _uri) ERC1155(_uri) {}
 
-    emit assetMinted(assetRef, assetTokenIdCount);
+  modifier isCreatorOrApproved(address account) {
+    require(account == msg.sender  || isApprovedForAll(account, msg.sender),
+        "ERC1155: caller is not creator nor approved");
+    _;
+  }
+
+  function mintAssetShares(
+    string memory _assetRef, 
+    string memory _uri) 
+    public  
+  {
+    require(!_isAssetSharesExists(_assetRef), "RECORD: Shares for asset allready minted");
+
+    assetRefs.push(_assetRef);
+
+    address[] memory adr;
+    AssetShareToken memory assetShareToken  = AssetShareToken(assetRefs.length, msg.sender, AssetShareTokenStatus.MINTED, 1000, _uri, adr);
+
+    assetShareTokens[_assetRef] = assetShareToken;
+
+    _tokenAssetRefs[assetShareToken.id] = _assetRef;
+
+
+    _mint(assetShareToken.creator, assetShareToken.id, assetShareToken.shares, ""); 
 
   }
 
-  function getAssetTokenIdForAsset(string memory assetRef) public view returns (uint256) {
-    uint256 assetTokenId = _mintedAssetTokens[assetRef];
-    return assetTokenId;
+  function devideAssetShares(
+      address creator,
+      address[] memory _shareHolders,
+      string memory _assetRef,
+      uint256[] memory _amounts,
+      bytes memory _data) 
+      public  
+      isCreatorOrApproved(creator)
+  {
+    require(_shareHolders.length == _amounts.length, "RECORD: ShareHolders and amounts length mismatch");
+    require(_isAssetSharesExists(_assetRef), "RECORD: Asset not minted yet");
+    require(!_isAssetSharesDevided(_assetRef), "RECORD: Shares for Asset allready devided");
+    require(_isAllSharesDevided(_amounts), "RECORD: All shares should be devided correctly");
+
+    assetShareTokens[_assetRef].status = AssetShareTokenStatus.DEVIDED;
+
+    for (uint256 i = 0; i < _shareHolders.length; i++) {
+      transfer(creator, _shareHolders[i], _assetRef, _amounts[i], _data);
+    }
+
   }
 
-  function assetAllReadyMinted(string memory assetRef) internal view returns (bool) {
-    return _assetTokenMinted[assetRef];
+  function transfer(
+      address _from, 
+      address _to, 
+      string memory _assetRef, 
+      uint256 _amount, 
+      bytes memory _data) 
+      public 
+  {
+    uint256 id = assetShareTokens[_assetRef].id;
+    super.safeTransferFrom(_from, _to, id, _amount, _data);
+  }
+
+  function transferBatch(
+      address _from,
+      address _to, 
+      string[] memory _assetRefs,
+      uint256[] memory _amounts,
+      bytes memory _data) 
+      public 
+  {
+    uint256[] memory ids = _idsByAssetRefs(_assetRefs);
+    super.safeBatchTransferFrom(_from, _to, ids, _amounts, _data);
+  }
+
+  function balance(
+      address _owner, 
+      string memory _asserRef) 
+      public 
+      view 
+      returns (uint256)  
+  {
+    return super.balanceOf(_owner, assetShareTokens[_asserRef].id);
+  }
+
+  function balanceBatch(
+    address[] memory _owners, 
+    string[] memory _assetRefs) 
+    public 
+    view 
+    returns (uint256[] memory)
+  {
+    return super.balanceOfBatch(_owners, _idsByAssetRefs(_assetRefs));
+  }
+
+  function mintedAssetSharesBy(
+    address creator)
+    public
+    view
+    isCreatorOrApproved(creator)
+    returns (string[] memory mintedAssets)
+
+  {
+    string[] memory created = new string[](assetRefs.length);
+
+    uint256 mintedCount = 0;
+
+    for (uint256 i = 0; i < assetRefs.length; i++) {
+      if (assetShareTokens[assetRefs[i]].status == AssetShareTokenStatus.MINTED && assetShareTokens[assetRefs[i]].creator == creator) {
+        created[mintedCount++] = assetRefs[i];
+      }
+    }
+
+    mintedAssets = new string[](mintedCount);
+
+    for (uint256 i = 0; i < mintedCount; i++) {
+      mintedAssets[i] = created[i];
+    }
+     
+  }
+
+  function allDevidedAssets()
+    public 
+    view 
+    returns (string[] 
+    memory devidedAssets) 
+  {
+    string[] memory created = new string[](assetRefs.length);
+
+    uint256 mintedCount = 0;
+
+    for (uint256 i = 0; i < assetRefs.length; i++) {
+      if (assetShareTokens[assetRefs[i]].status == AssetShareTokenStatus.DEVIDED) {
+        created[mintedCount++] = assetRefs[i];
+      }
+    }
+
+    devidedAssets = new string[](mintedCount);
+
+    for (uint256 i = 0; i < mintedCount; i++) {
+      devidedAssets[i] = created[i];
+    }
+  }
+
+  function shareHoldersWithBalance(
+    string memory _assetRef) 
+    public view 
+    returns (address[] memory,
+    uint256[] memory)
+  {
+    require(_isAssetSharesExists(_assetRef), "RECORD: Asset not minted yet");
+
+    AssetShareToken memory share = assetShareTokens[_assetRef];
+
+    address[] memory holdersWithBalanceTemp = new address[](share.holders.length);
+    uint256[] memory amountsOfHoldersWithBalanceTemp = new uint256[](share.holders.length);
+
+    uint holderWithBalanceCount = 0;
+
+    for (uint256 i=0; i< share.holders.length; i++ ) {
+      uint256 balanceForHolder = balance(share.holders[i], _assetRef);
+      if (balanceForHolder > 0) {
+        holdersWithBalanceTemp[holderWithBalanceCount] = share.holders[i];
+        amountsOfHoldersWithBalanceTemp[holderWithBalanceCount] = balanceForHolder;
+        holderWithBalanceCount++;
+      }
+    }
+
+    address[] memory holdersWithBalance = new address[](holderWithBalanceCount);
+    uint256[] memory amountsOfHoldersWithBalance = new uint256[](holderWithBalanceCount);
+
+    for (uint256 i=0; i< holderWithBalanceCount; i++ ) {
+        holdersWithBalance[i] = holdersWithBalanceTemp[i];
+        amountsOfHoldersWithBalance[i] = amountsOfHoldersWithBalanceTemp[i];
+    }
+
+    return (holdersWithBalance, amountsOfHoldersWithBalance);
+
+  }
+
+  function metadataUri(string memory _assetRef)
+      public 
+      view 
+      isCreatorOrApproved(assetShareTokens[_assetRef].creator) 
+      returns (string memory) 
+  {
+        return assetShareTokens[_assetRef].metadataUri;
+  }
+
+  function allAssets()
+  public
+  view
+  returns (string[] memory)
+  {
+    return assetRefs;
   }
 
   function _beforeTokenTransfer(
@@ -67,58 +233,58 @@ contract Record is ERC1155  {
     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     
     for (uint i = 0; i < ids.length; i++) {
-      if (_isAssetTokenIdHolder[ids[i]][to] != true) {
-         _assetTokenIdHolder[ids[i]][_assetTokenIdHolderCount[ids[i]]] = to;
-        _assetTokenIdHolderCount[ids[i]] = _assetTokenIdHolderCount[ids[i]].add(1);
+      if (_ownedAssetShares[to][ids[i]] != true) {
+         string memory ref = _tokenAssetRefs[ids[i]];
+         assetShareTokens[ref].holders.push(to);
+         _ownedAssetShares[to][ids[i]] = true;
       }
     }
   }
-  
 
-  function getAssetTokenHolders (
-        string memory assetRef
-    )
-        public
-        view
-        
-        returns (address[] memory)
-    {
-        uint256 assetTokenId = _mintedAssetTokens[assetRef];
-        address[] memory assetTokenHolders = new address[](_assetTokenIdHolderCount[assetTokenId]);
-
-        for (uint256 i = 0 ; i < _assetTokenIdHolderCount[assetTokenId] ; ++i) {
-            
-            assetTokenHolders[i] = _assetTokenIdHolder[assetTokenId][i];
-        }
-
-        return assetTokenHolders;
+  function _idsByAssetRefs(
+    string[] memory _assetRefs) 
+    internal 
+    view
+    returns (uint256[] memory ids)
+  {
+    ids = new uint256[](_assetRefs.length);
+    
+    for (uint256 i = 0; i < _assetRefs.length; i++) {
+      ids[i] = assetShareTokens[_assetRefs[i]].id;
     }
-
-  function getAssetTokenHolderCount (
-        string memory assetRef
-    )
-        public
-        view
-        
-        returns (uint256)
-    {
-        uint256 assetTokenId = _mintedAssetTokens[assetRef];
-        return _assetTokenIdHolderCount[assetTokenId];
   }
 
-  function getMintedAssets ()
-        public
-        view
-        
-        returns (string[] memory)
-    {
-        string[] memory mintedAssets = new string[](_assetTokenIdCount);
+  function _isAssetSharesExists(
+    string memory _assetRef) 
+    internal 
+    view 
+    returns (bool) 
+  {
+    return assetShareTokens[_assetRef].id > 0;
+  }
 
-        for (uint256 i = 0 ; i < _assetTokenIdCount ; ++i) {
-            mintedAssets[i] = _mintedAssetTokensRefs[i];
-        }
+  function _isAssetSharesDevided(
+    string memory _assetRef) 
+    internal 
+    view 
+    returns (bool) 
+  {
+    return assetShareTokens[_assetRef].status == AssetShareTokenStatus.DEVIDED;
+  }
 
-        return mintedAssets;
+  function _isAllSharesDevided(
+    uint256[] memory _amounts) 
+    internal 
+    pure 
+    returns (bool) 
+  {
+    uint256 counter = 0;
+
+    for (uint256 i = 0; i < _amounts.length; i++) {
+      counter = counter + _amounts[i];
+    }
+
+    return counter == 1000;
   }
 
 }
--- record-Dapp/record-contract/test/Record.js
+++ record-Dapp/record-contract/test/Record.js
@@ -1,113 +1,312 @@
 const { ethers } = require('hardhat');
-const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
-const { accounts } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
 
 before(async () => {
-  const [signer] = await ethers.getSigners();
-  console.log('signer:', signer.address);
-  this.Record = await ethers.getContractFactory('Record');
+
+  const [administrator, owner1, owner2, buyer1, buyer2] = await ethers.getSigners();
+
+  this.Record = await ethers.getContractFactory('Record');`
+ `
   this.accounts = {
-    administrator: signer.address,
-    owner1: accounts[0],
-    owner2: accounts[1],
+    administrator: administrator,
+    owner1: owner1,
+    owner2: owner2,
+    buyer1: buyer1,
+    buyer2: buyer2,
   };
-  console.log('accounts:', this.accounts);
+
 });
 
 beforeEach(async () => {
-  this.record = await this.Record.deploy('URI{id}');
+  this.record = await this.Record.deploy('https://energyweb.org/');
   await this.record.deployed();
-  await this.record.mintAsset('Asset1');
+  await this.record.mintAssetShares('Asset1', 'URI1');
 });
 
 describe('Record', () => {
-  it('Should return the Uri', async () => {
-    expect(await this.record.uri(0)).to.equal('URI{id}');
-  });
-
-  it('Should return balance 1000 for new minted asset token', async () => {
+  it('Should return balance of 1000 for new minted asset share', async () => {
     expect(
-      await this.record.balanceOf(this.accounts.administrator, 0)
+      await this.record.balance(this.accounts.administrator.address, 'Asset1')
     ).to.equal(1000);
   });
 
   it('Should return balance 1000 for another new minted asset token', async () => {
-    await this.record.mintAsset('Asset2');
-
+    await this.record.mintAssetShares('Asset2', 'URI2');
     expect(
-      await this.record.balanceOf(this.accounts.administrator, 1)
+        await this.record.balance(this.accounts.administrator.address, 'Asset2')
     ).to.equal(1000);
   });
 
-  it('Should revert when token allready minted!', async () => {
-    await expectRevert(
-      this.record.mintAsset('Asset1'),
-      'Asset with with DID is allready minted!'
-    );
-  });
 
-  it('Should transfer 100 asset shares from administrator to owner1!', async () => {
-    await this.record.safeTransferFrom(
-      this.accounts.administrator,
-      this.accounts.owner1,
-      0,
-      100,
-      []
-    );
+  it('Should return balances for owners and minted asset tokens', async () => {
+    await this.record.mintAssetShares('Asset2', 'URI2');
+
+    const owners = [this.accounts.administrator.address, this.accounts.administrator.address];
+    const assets = ["Asset1","Asset2"];
+
+    const balances = await this.record.balanceBatch(owners, assets);
 
     expect(
-      await this.record.balanceOf(this.accounts.administrator, 0)
-    ).to.equal(900);
-    expect(await this.record.balanceOf(this.accounts.owner1, 0)).to.equal(100);
+        balances[0]
+    ).to.equal(1000);
+
+    expect(
+        balances[1]
+    ).to.equal(1000);
   });
 
-  it('Should revert sending 1001 asset shares from administrator to owner1 with balance of 1000!', async () => {
-    await expectRevert(
-      this.record.safeTransferFrom(
-        this.accounts.administrator,
-        this.accounts.owner1,
-        0,
-        1001,
-        []
-      ),
-      'ERC1155: insufficient balance for transfer'
+
+  it('Should return balance of 1000 for new minted asset share', async () => {
+    expect(
+        await this.record.balance(this.accounts.administrator.address, 'Asset1')
+    ).to.equal(1000);
+  });
+
+  it('Should return mintedAsset for new minted asset share', async () => {
+    const mintedAsset = await this.record.assetShareTokens('Asset1');
+
+    expect(mintedAsset['id']).to.equal(1);
+    expect(mintedAsset['creator']).to.equal(this.accounts.administrator.address);
+    expect(mintedAsset['status']).to.equal(0);
+    expect(mintedAsset['shares']).to.equal(1000);
+    expect(mintedAsset['metadataUri']).to.equal('URI1');
+  });
+
+  it('Should return holder with balance for new minted asset share', async () => {
+    const shareHoldersWithalance = await this.record.shareHoldersWithBalance('Asset1');
+
+    expect(shareHoldersWithalance[0][0]).to.equal(this.accounts.administrator.address);
+    expect(shareHoldersWithalance[1][0]).to.equal(1000);
+
+  });
+
+
+  it('Should return mintedAsset for another minted asset share', async () => {
+    await this.record.mintAssetShares('Asset2', 'URI2');
+    const mintedAsset = await this.record.assetShareTokens('Asset2');
+
+    expect(mintedAsset['id']).to.equal(2);
+    expect(mintedAsset['creator']).to.equal(this.accounts.administrator.address);
+    expect(mintedAsset['status']).to.equal(0);
+    expect(mintedAsset['shares']).to.equal(1000);
+    expect(mintedAsset['metadataUri']).to.equal('URI2');
+  });
+
+  it('Should return all holders with balance when another asset share was minted', async () => {
+    await this.record.mintAssetShares('Asset2', 'URI2');
+
+    const shareHoldersWithalanceForAsset1 = await this.record.shareHoldersWithBalance('Asset1');
+
+    const shareHoldersWithalanceForAsset2 = await this.record.shareHoldersWithBalance('Asset2');
+
+    expect(shareHoldersWithalanceForAsset1[0][0]).to.equal(this.accounts.administrator.address);
+    expect(shareHoldersWithalanceForAsset1[1][0]).to.equal(1000);
+
+    expect(shareHoldersWithalanceForAsset2[0][0]).to.equal(this.accounts.administrator.address);
+    expect(shareHoldersWithalanceForAsset2[1][0]).to.equal(1000);
+
+  });
+
+  it('Should return metadataUri for minted Asset share', async () => {
+
+    const uri = await this.record.connect(this.accounts.administrator).metadataUri('Asset1');
+
+    expect(await this.record.metadataUri('Asset1')).to.equal('URI1');
+  });
+
+  it('Should return all minted assets', async () => {
+    await this.record.mintAssetShares('Asset2', 'URI2');
+    await this.record.mintAssetShares('Asset3', 'URI3');
+
+    const assets = await this.record.allAssets();
+
+    expect(assets[0]).to.equal('Asset1');
+    expect(assets[1]).to.equal('Asset2');
+    expect(assets[2]).to.equal('Asset3');
+
+  });
+
+  it('Should return all minted assets by address', async () => {
+    await this.record.mintAssetShares('Asset2', 'URI2');
+
+    const assets = await this.record.mintedAssetSharesBy(this.accounts.administrator.address);
+
+    expect(assets[0]).to.equal('Asset1');
+    expect(assets[1]).to.equal('Asset2');
+
+  });
+
+  it('Should devide shares for asset to share holders', async () => {
+    const shareHolders = [this.accounts.owner1.address, this.accounts.owner2.address];
+    const amounts = [600, 400];
+
+    await this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHolders, 'Asset1', amounts, []);
+
+    expect(
+        await this.record.connect(this.accounts.administrator).balance(this.accounts.owner1.address, 'Asset1')
+    ).to.equal(600);
+
+    expect(
+        await this.record.connect(this.accounts.administrator).balance(this.accounts.owner2.address, 'Asset1')
+    ).to.equal(400);
+
+  });
+
+  it('Should get asset holders after devide shares for asset ', async () => {
+    const shareHolders = [this.accounts.owner1.address, this.accounts.owner2.address];
+    const amounts = [600, 400];
+
+    await this.record.devideAssetShares(this.accounts.administrator.address, shareHolders, 'Asset1', amounts, []);
+
+  });
+
+  it('Should return all divided assets', async () => {
+    await this.record.connect(this.accounts.administrator).mintAssetShares('Asset2', 'URI2');
+    const shareHoldersAsset1 = [this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset1 = [600, 400];
+
+    await this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset1, 'Asset1', amountsAsset1, []);
+
+    const shareHoldersAsset2 = [this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset2 = [600, 400];
+
+    await this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset2, 'Asset2', amountsAsset2, []);
+
+    const allDevidedAssets = await this.record.allDevidedAssets();
+
+    expect(
+        allDevidedAssets[0]
+
+    ).to.equal('Asset1');
+
+    expect(
+        allDevidedAssets[1]
+    ).to.equal('Asset2');
+
+  });
+
+  it('Should return all holders for devided asset shares', async () => {
+
+    const shareHoldersAsset1 = [this.accounts.administrator.address, this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset1 = [100, 600, 300];
+
+    await this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset1, 'Asset1', amountsAsset1, []);
+
+    const shareHoldersWithalance = await this.record.shareHoldersWithBalance('Asset1');
+
+    expect(shareHoldersWithalance[0][0]).to.equal(this.accounts.administrator.address);
+    expect(shareHoldersWithalance[1][0]).to.equal(100);
+
+    expect(shareHoldersWithalance[0][1]).to.equal(this.accounts.owner1.address);
+    expect(shareHoldersWithalance[1][1]).to.equal(600);
+
+    expect(shareHoldersWithalance[0][2]).to.equal(this.accounts.owner2.address);
+    expect(shareHoldersWithalance[1][2]).to.equal(300);
+
+  });
+
+  it('Should return all holders for transfered asset shares', async () => {
+
+    const shareHoldersAsset1 = [this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset1 = [600, 400];
+
+    await this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset1, 'Asset1', amountsAsset1, []);
+
+    await this.record.connect(this.accounts.owner1).transfer(this.accounts.owner1.address, this.accounts.buyer1.address, 'Asset1', 100, []);
+
+    const shareHoldersWithalance = await this.record.shareHoldersWithBalance('Asset1');
+
+    expect(shareHoldersWithalance[0][0]).to.equal(this.accounts.owner1.address);
+    expect(shareHoldersWithalance[1][0]).to.equal(500);
+
+    expect(shareHoldersWithalance[0][1]).to.equal(this.accounts.owner2.address);
+    expect(shareHoldersWithalance[1][1]).to.equal(400);
+
+    expect(shareHoldersWithalance[0][2]).to.equal(this.accounts.buyer1.address);
+    expect(shareHoldersWithalance[1][2]).to.equal(100);
+
+  });
+
+  it('Should revert when token allready minted!', async () => {
+    await this.expectRevert(
+        this.record.connect(this.accounts.administrator).mintAssetShares('Asset1', 'URI1')
+        , 'RECORD: Shares for asset allready minted');
+  });
+
+  it('Should revert when get holders with balance for asset not minted yet!', async () => {
+
+    await this.expectRevert(
+        this.record.shareHoldersWithBalance('Asset2'),
+        'RECORD: Asset not minted yet'
     );
   });
 
-  it('Should get tokenId for Asset1 !', async () => {
-    expect(await this.record.getAssetTokenIdForAsset('Asset1')).to.equal(0);
-  });
+    it('Should revert devide shares for asset to share holders when shareholders and amounts lenght mismatch', async () => {
+      const shareHolders = [this.accounts.administrator.address];
+      const amounts = [500, 500];
 
-  it('Should show all asset token holders for Asset1 after transfer from administrator to owner1 and owner2!', async () => {
-    await this.record.safeTransferFrom(
-      this.accounts.administrator,
-      this.accounts.owner1,
-      0,
-      100,
-      []
+      await this.expectRevert(
+          this.record.devideAssetShares(this.accounts.administrator.address, shareHolders, 'Asset1', amounts, []),
+          'RECORD: ShareHolders and amounts length mismatch'
+      );
+
+    });
+
+    it('Should revert devide shares for asset to share holders when asset is not minted yet', async () => {
+      const shareHolders = [this.accounts.administrator.address, this.accounts.administrator.address];
+      const amounts = [500, 500];
+
+      await this.expectRevert(
+          this.record.devideAssetShares(this.accounts.administrator.address, shareHolders, 'Asset2', amounts, []),
+          'RECORD: Asset not minted yet'
+      );
+
+    });
+
+  it('Should revert devide shares for asset to share holders when not correctly devided: > 1000', async () => {
+    const shareHoldersAsset1 = [this.accounts.administrator.address, this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset1 = [1, 600, 400];
+
+    await this.expectRevert(
+        this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset1, 'Asset1', amountsAsset1, []),
+        'RECORD: All shares should be devided correctly'
     );
 
-    await this.record.safeTransferFrom(
-      this.accounts.administrator,
-      this.accounts.owner2,
-      0,
-      100,
-      []
-    );
-
-    const [holder1, holder2, holder3] = await this.record.getAssetTokenHolders(
-      'Asset1'
-    );
-
-    expect(holder1).to.equal(this.accounts.administrator);
   });
 
-  it('Should return Asset1 and Asset2 as minted assets', async () => {
-    await this.record.mintAsset('Asset2');
+  it('Should revert devide shares for asset to share holders when not correctly devided: < 1000', async () => {
+    const shareHoldersAsset1 = [this.accounts.administrator.address, this.accounts.owner1.address, this.accounts.owner2.address];
+    const amountsAsset1 = [99, 600, 300];
 
-    const mintedAssets = await this.record.getMintedAssets();
-    expect(mintedAssets[0]).to.equal('Asset1');
-    expect(mintedAssets[1]).to.equal('Asset2');
+    await this.expectRevert(
+        this.record.connect(this.accounts.administrator).devideAssetShares(this.accounts.administrator.address, shareHoldersAsset1, 'Asset1', amountsAsset1, []),
+        'RECORD: All shares should be devided correctly'
+    );
+
   });
+
+  it('Should revert when mintedAssetSharesBy not called by creator', async () => {
+
+    await this.expectRevert(
+        this.record.connect(this.accounts.owner1).mintedAssetSharesBy(this.accounts.administrator.address),
+        'ERC1155: caller is not creator nor approved'
+    );
+
+  });
+
+  this.expectRevert = async (promise, expectedError) => {
+    try {
+      await promise;
+    } catch(error) {
+
+      const actualError = error.message.replace(
+          'VM Exception while processing transaction: revert ',
+          '',
+      );
+      expect(actualError).to.equal(expectedError);
+      return;
+    }
+    expect.fail('Exception expected');
+  }
+
 });
